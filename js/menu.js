6f35b6e3e0a67e2e205216467c7e56612445a3e0
galinov <galinov@umisoft.ru>
2016-10-24 16:28:03 +0300

    header_mobile + footer

---
+++ js/menu.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    // адаптивное меню
+    initHideELement($('#topNav'), $('#navToggle'), $('#closeToggle'), 'active');
+
+    function initHideELement(element, openToggle, closeToggle, newClass) {
+        function initCollapse() {
+            if (element.hasClass(newClass)) {
+                element.removeClass(newClass);
+            } else {
+                element.addClass(newClass);
+            };
+        }
+        openToggle.click(function(event) {
+            initCollapse();
+            event.preventDefault();
+        });
+        openToggle.on('tap', function(event)  {
+            initCollapse();
+            event.preventDefault();
+        });
+        if (closeToggle != false) {
+            closeToggle.click(function(event) {
+                initCollapse();
+                event.preventDefault();
+            });
+            closeToggle.on('tap', function(event) {
+                initCollapse();
+                event.preventDefault(event);
+            });
+        };
+    }
+});
