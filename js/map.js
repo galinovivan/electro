444f5e9816744a1102be703c43dfc8462458b628
galinov <galinov@umisoft.ru>
2016-11-07 10:40:54 +0300

    fix

--- js/map.js
+++ js/map.js
@@ -2,14 +2,97 @@
 function initMap() {
     var latitude = 60.096856,
         longitude = 30.150632,
-        map_zoom = 20;
-    var style = [{
-        featureType: 'all',
-        stylers: [
-            {saturation: -100}]
-    },{featureType: 'water',
-        skylers: [
-            {saturation: 1}]}];
+        map_zoom = 10;
+    var style = [
+        {
+            "featureType": "all",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "saturation": "-100"
+                }
+            ]
+        },
+        {
+            "featureType": "administrative",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#444444"
+                }
+            ]
+        },
+        {
+            "featureType": "landscape",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "color": "#f2f2f2"
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "saturation": -100
+                },
+                {
+                    "lightness": 45
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "visibility": "simplified"
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "labels.icon",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "transit",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "all",
+            "stylers": [
+                {    "color": "#c8e6f4"},
+                {
+                    "visibility": "on"
+                },
+                {
+                    "saturation": "100"
+                }
+            ]
+        }
+    ];
     var map_options = {
         center: new google.maps.LatLng(latitude, longitude),
         zoom: map_zoom,
@@ -20,7 +103,7 @@ function initMap() {
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         scrollwheel: true,
         styles: style
-    }
+    };
     var map = new google.maps.Map(document.getElementById('map'), map_options);
     var image = 'img/map_marker.png';
     var beachMarker = new google.maps.Marker({
